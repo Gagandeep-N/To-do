808544549f9d5d4b1c422b9c11aee6a0e616c0c9
Gagan-ELS <g.nandakishore@elsevier.com>
2024-03-09 15:14:30 +0530

    Added functionality for delete button

--- to-do/src/components/Todo.js
+++ to-do/src/components/Todo.js
@@ -1,29 +1,35 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import TodoItem from "./TodoItem";
 import '../assets/Todo.css'
 
 const Todo = () => {
 
     const [task, setTask] = useState('');
-    const [tasks,setTasks] = useState([]);
+    const [tasks, setTasks] = useState([]);
 
     const handleClick = () => {
-        if (task.trim() !== ''){
-            setTasks((prevTask)=>
-                [...prevTask,task]
+        if (task.trim() !== '') {
+            setTasks((prevTask) =>
+                [...prevTask, task]
             );
             setTask('');
         }
     }
 
-    return(
+    const handleDelete = (index) => {
+        setTasks(prevTasks => prevTasks.filter((_, i) => i !== index));
+    }
+
+
+
+    return (
         <div className="todo-container">
             <h1 className="heading">TODO LIST</h1>
             <div className="input-container">
-            <input placeholder="Enter your task..." value={task} onChange={(e)=>setTask(e.target.value)} className="task-input"/>
-            <button onClick={handleClick} className="add-button">Add Task</button>
+                <input placeholder="Enter your task..." value={task} onChange={(e) => setTask(e.target.value)} className="task-input" />
+                <button onClick={handleClick} className="add-button">Add Task</button>
             </div>
-            <TodoItem tasks={tasks}/>
+            <TodoItem tasks={tasks} onDelete={handleDelete} />
         </div>
     )
 }
--- to-do/src/components/TodoItem.js
+++ to-do/src/components/TodoItem.js
@@ -2,22 +2,21 @@ import React from "react";
 import '../assets/TodoItem.css';
 import binImage from '../assets/Images/bin.png';
 
-const TodoItem = ({tasks}) => {
+const TodoItem = ({ tasks, onDelete }) => {
+
     return (
         <div className="tasks-conatiner">
-            
-                {tasks.map((task, index) => (
-                    <div className="tasks-list">
-                        <div className="check-task">
-                        <div><input type="checkbox"/></div>
-                    <div key={index}>{task}</div>
-                    </div>
 
-
-                    <div className="delete"> <button className="delete-button"><img alt='delete' src={binImage} height="16px" width="16px"/></button> </div>
+            {tasks.map((task, index) => (
+                <div className="tasks-list">
+                    <div className="check-task">
+                        <div><input type="checkbox" /></div>
+                        <div key={index}>{task}</div>
                     </div>
-                ))}
-            
+                    <div className="delete"> <button className="delete-button" onClick={() => onDelete(index)}><img alt='delete' src={binImage} height="16px" width="16px" /></button> </div>
+                </div>
+            ))}
+
 
         </div>
     )
