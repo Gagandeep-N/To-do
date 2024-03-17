c45996ddc3e01ac80d61f9ba4d425e909e2b852d
Gagan-ELS <g.nandakishore@elsevier.com>
2024-03-17 11:43:21 +0530

    Added strikethrough functionality

--- to-do/src/components/TodoItem.js
+++ to-do/src/components/TodoItem.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import '../assets/TodoItem.css';
 import binImage from '../assets/Images/bin.png';
 
 const TodoItem = ({ tasks, onDelete }) => {
 
+    const [checked, setChecked] = useState(new Array(tasks.length).fill(false));
+
+    const checkHandler = (index) => {
+        setChecked(prev => {
+            const newList = [...prev];
+            newList[index] = !newList[index];
+            return newList;
+        });
+    }
+
     return (
         <div className="tasks-conatiner">
-
+            
             {tasks.map((task, index) => (
                 <div className="tasks-list">
                     <div className="check-task">
-                        <div><input type="checkbox" /></div>
-                        <div key={index}>{task}</div>
+                        <div><input type="checkbox" onClick={() => checkHandler(index)} /></div>
+                        <div key={index} style={{ textDecoration: checked[index] ? "line-through" : "none" }}>{task}</div>
                     </div>
                     <div className="delete"> <button className="delete-button" onClick={() => onDelete(index)}><img alt='delete' src={binImage} height="16px" width="16px" /></button> </div>
                 </div>
             ))}
-
 
         </div>
     )
